c6d66bfb1219ce160742501201309b5dfd024fb1
Wendreo <wendreolf@gmail.com>
2019-06-17 21:42:53 -0300

    Adding files

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -23,64 +23,64 @@ class Feed extends Component {
         this.setState({feed: response.data})
     }
 
-    registerToSocket = () =>{
+    registerToSocket = () => {
         const socket = io('http://localhost:3333');
 
-        socket.on('post', newPost =>{
-            this.setState({feed: [newPost, ... this.state.feed] })
+        socket.on('post', newPost => {
+            this.setState( {feed: [newPost, ...this.state.feed]} )
         })
 
-        socket.on('like', likedPost => {
-            this.setState({
-                feed: this.state.feed.map(post =>
-                post.id === likedPost._id? likedPost: post
-            )
-            })
+        socket.on('like', likePost => {
+            this.setState( {
+                feed: this.state.feed.map(
+                    post => post._id === likePost._id ? likePost : post
+                )
+            } )
         })
     }
 
     handleLike = id => {
-        api.post(`/posts/${id}/like`)
-    };
+        api.post(`/posts/${id}/like`);
+    }
 
     render() {
         return (
             <section id="post-list">
-
                 {this.state.feed.map(post => (
-                    <article key={post.id}>
+                    <article key={post._id}>
                         <header>
                             <div className="user-info">
                                 <span>{post.author}</span>
                                 <span className="place">{post.place}</span>
                             </div>
 
-                            <img src={more} alt="Mais"/>
+                            <img src={more} alt="Mais" />
                         </header>
 
-                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
                         <footer>
                             <div className="actions">
-                                <button type="button" onClick={() => this.handleLike}>
-                                <img src={like} alt=""/>
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="" />
                                 </button>
-                                <img src={comment} alt=""/>
-                                <img src={send} alt=""/>
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
                             </div>
 
                             <strong>{post.likes} curtidas</strong>
 
-                            <p> {post.description}
+                            <p>{post.description}
                                 <span>{post.hashtags}</span>
                             </p>
 
                         </footer>
+
                     </article>
                 ))}
-
             </section>
         );
+
     }
 }
 
